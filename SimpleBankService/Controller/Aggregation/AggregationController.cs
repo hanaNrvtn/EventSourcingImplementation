e50d496542880b127005245db679ad944118e9f7
HanaNrvtn <hanaNrvtn@gmail.com>
2024-08-04 10:50:42 -0700

    just rename files.

---
+++ SimpleBankService/Controller/Aggregation/AggregationController.cs
@@ -0,0 +1,40 @@
+using System;
+using ConsoleApplication1.Assert.Database;
+using ConsoleApplication1.Controller.Aggregation.Abstraction;
+using ConsoleApplication1.Controller.Event;
+using ConsoleApplication1.Model.Event;
+using EventSourcingImplementation.Assert.Database;
+
+namespace ConsoleApplication1.Controller.Aggregation
+{
+    public class AggregationController : IAggregationController
+    {
+        private readonly EventController _eventController;
+        private readonly AccountDbManager _accountDbManager;
+        public AggregationController()
+        {
+            _accountDbManager = new AccountDbManager();
+            _eventController = new EventController();
+        }
+
+        public void ValidateWithdrawEvent(string id, double amount)
+        {
+            var balance = _accountDbManager.Get(id).Balance;
+            
+            if (balance < amount)
+                _eventController.AddHitWithdrawLimitEvent(new HitWithdrawLimit(Guid.NewGuid().ToString(), id, "withdrawHitLimit", "dkfl"));
+            else        
+                _eventController.AddWithdrawMoneyEvent(new WithdrawMoneyEvent(Guid.NewGuid().ToString(), id, "withdraw event", "dfkjdkfj"), amount);
+        }
+        
+        public void ValidateDepositEvent(string id, double amount)
+        {
+            var balance = _accountDbManager.Get(id).Balance;
+            
+            if (balance < 1000 && amount > 1000000)
+                _eventController.AddHitDepositLimitEvent(new HitDepositLimit(Guid.NewGuid().ToString(), id, "withdrawHitLimit", "dkfl"));
+            else
+                _eventController.AddDepositMoneyEvent(new DepositMoneyEvent(Guid.NewGuid().ToString(), id, "withdraw event", "dfkjdkfj"), amount);
+        }
+    }
+}
---
+++ SimpleBankService/Controller/Projector/Projector.cs
@@ -0,0 +1,32 @@
+using System;
+using ConsoleApplication1.Assert.Database;
+using ConsoleApplication1.Model.Event;
+using EventSourcingImplementation.Assert.Database;
+
+namespace ConsoleApplication1.Controller.Projector
+{
+    public class Projector
+    {
+        private readonly AccountDbManager _accountDbManager;
+
+        public Projector(AccountDbManager accountDbManager)
+        {
+            _accountDbManager = accountDbManager;
+        }
+
+        public void OnAccountCreated(CreateAccountEvent createAccountEvent)
+        {
+            _accountDbManager.Add(createAccountEvent.AccountId,0, DateTime.Now, DateTime.Now);
+        }
+        
+        public void OnDeposit(DepositMoneyEvent depositMoneyEvent, double amount)
+        { 
+            _accountDbManager.Update(depositMoneyEvent.AccountId, _accountDbManager.Get(depositMoneyEvent.AccountId).Balance + amount);
+        }
+        
+        public void OnWithdraw(WithdrawMoneyEvent withdrawMoneyEvent, double amount)
+        {
+            _accountDbManager.Update(withdrawMoneyEvent.AccountId, _accountDbManager.Get(withdrawMoneyEvent.AccountId).Balance - amount);
+        }
+    }
+}
